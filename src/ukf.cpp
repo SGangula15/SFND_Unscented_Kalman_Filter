7292e7db472123a2ba038faa488a035f05c3cd3e
SGangula15 <graduate.srikanth@gmail.com>
2020-04-28 15:40:28 -0400

    Functional Code#1: Initialise sigma pts and weights

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -54,6 +54,31 @@ UKF::UKF() {
    * TODO: Complete the initialization. See ukf.h for other member properties.
    * Hint: one or more values initialized above might be wildly off...
    */
+
+  // initially set to false, set to true in first call of ProcessMeasurement
+  is_initialized_ = false;
+
+  // State dimension
+  n_x_ = 5;
+
+  // Augmented state dimension
+  n_aug_ = 7;
+
+  // Sigma point spreading parameter
+  lambda_ = 3 - n_x_;
+
+  // predicted sigma points matrix
+  Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);  
+  Xsig_pred_.fill(0.0);
+
+  // last step time in us
+  time_us_ = 0.0;
+
+  // Weights of sigma points
+  weights_ = VectorXd(2 * n_aug_ + 1);
+  weights_.fill(0.5 / (lambda_ + n_aug_));
+  weights_(0) = lambda_ / (lambda_ + n_aug_);
+
 }
 
 UKF::~UKF() {}
